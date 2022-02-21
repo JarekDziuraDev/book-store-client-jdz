e9de71214d80f2c76894482559d02818efcec4f5
JarekDziuraDev <jarek.dziura.dev@gmail.com>
2022-02-21 12:44:25 +0100

    add book display group components

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import axios from 'axios';
 import baseUrl from '../config';
 import Layout from "./layout/Layout";
+import BookContainer from "./book/BookContainer";
 
 const App = ()  => {
 
@@ -12,7 +13,7 @@ const App = ()  => {
 
   return (
     <Layout>
-      <div>My first component</div>
+      <BookContainer />
     </Layout>
   );
 }
